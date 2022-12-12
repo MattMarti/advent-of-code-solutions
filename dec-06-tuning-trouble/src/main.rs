78e8fa99678c23d5b6fa38ae296ff612b95cc6cc
MattMarti <matthew.marti@gmail.com>
2022-12-11 23:07:47 -0500

    Sovled part one of Dec 6 challenge. Find index where signal starts

--- dec-06-tuning-trouble/src/main.rs
+++ dec-06-tuning-trouble/src/main.rs
@@ -2,14 +2,42 @@ use std::env;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
+#[derive(Default)]
 struct SignalSlider {
-
+    pub idx: usize,
+    buf: [char; 4],
 }
 
-fn find_packet_start(signal: &str) -> usize {
+impl SignalSlider {
+    fn push(&mut self, c: char) {
+        self.buf[self.idx % 4] = c;
+        self.idx += 1;
+    }
 
+    fn locked(&self) -> bool {
+        for (i, &left) in self.buf.iter().enumerate() {
+            for j in i+1..self.buf.len() {
+                if left == self.buf[j] {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
 
-    0
+fn find_packet_start(signal: &Vec<char>) -> usize {
+    let mut slider = SignalSlider::default();
+    for i in 0..4 {
+        slider.push(signal[i]);
+    }
+    for i in 4..signal.len() {
+        slider.push(signal[i]);
+        if slider.locked() {
+            break;
+        }
+    }
+    slider.idx
 }
 
 fn main() -> io::Result<()> {
@@ -21,7 +49,7 @@ fn main() -> io::Result<()> {
 
     for read_line in reader.lines() {
         let line = read_line?;
-        println!("Signal start: {}", find_packet_start(&line));
+        println!("Signal start: {}", find_packet_start(&line.chars().collect()));
     }
 
     Ok(())
