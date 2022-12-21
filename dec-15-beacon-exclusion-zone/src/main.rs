ad737c684d3a605118743699aecee44ad769c362
Matt Mart <matthew.marti@gmail.com>
2022-12-21 16:55:39 -0500

    Dec 15 part 2. Find the location of the missing beacon

--- dec-15-beacon-exclusion-zone/src/main.rs
+++ dec-15-beacon-exclusion-zone/src/main.rs
@@ -1,7 +1,6 @@
-use env_logger;
 use lazy_static::lazy_static;
 use log::LevelFilter;
-use log::{debug, info, trace};
+use log::{debug, error, info, trace};
 use regex::Regex;
 use std::env;
 use std::fs::File;
@@ -9,79 +8,50 @@ use std::io::{prelude::*, BufReader};
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 struct Point {
-    x: i32,
-    y: i32,
+    x: i64,
+    y: i64,
 }
 
 #[derive(Clone, Copy)]
 struct Bounds {
-    min_x: i32,
-    max_x: i32,
-    min_y: i32,
-    max_y: i32,
-}
-
-impl Bounds {
-    pub fn from_points(points: &[Point]) -> Self {
-        let mut min_x: i32 = std::i32::MAX;
-        let mut max_x: i32 = std::i32::MIN;
-        let mut min_y: i32 = std::i32::MAX;
-        let mut max_y: i32 = std::i32::MIN;
-        for p in points.iter() {
-            if p.x < min_x {
-                min_x = p.x;
-            }
-            if max_x < p.x {
-                max_x = p.x;
-            }
-            if p.y < min_y {
-                min_y = p.y;
-            }
-            if max_y < p.y {
-                max_y = p.y;
-            }
-        }
-        Self {
-            min_x,
-            max_x,
-            min_y,
-            max_y,
-        }
-    }
+    min_x: i64,
+    max_x: i64,
+    min_y: i64,
+    max_y: i64,
 }
 
 #[derive(Debug)]
 struct Sensor {
     loc: Point,
-    range: i32,
+    range: i64,
 }
 
 impl Sensor {
-    pub fn min_x(&self, row: i32) -> i32 {
+    pub fn min_x(&self, row: i64) -> i64 {
         self.loc.x - self.row_range(row)
     }
 
-    pub fn max_x(&self, row: i32) -> i32 {
+    pub fn max_x(&self, row: i64) -> i64 {
         self.loc.x + self.row_range(row)
     }
 
-    pub fn row_range(&self, row: i32) -> i32 {
+    pub fn row_range(&self, row: i64) -> i64 {
         self.range - (row - self.loc.y).abs()
     }
 }
 
 #[derive(Debug, Copy, Clone)]
 struct Range {
-    start: i32,
-    end: i32,
+    start: i64,
+    end: i64,
 }
 
-fn in_range(start: i32, x: i32, end: i32) -> bool {
+fn in_range(start: i64, x: i64, end: i64) -> bool {
     start <= x && x <= end
 }
 
 impl Range {
-    pub fn from_sensor_row(sensor: &Sensor, row: i32) -> Option<Self> {
+    pub fn from_sensor_row(sensor: &Sensor, row: i64) -> Option<Self> {
         if sensor.row_range(row) < 0 {
             return None;
         }
@@ -121,13 +91,10 @@ fn get_overlapping_ranges(ranges: &[Range]) -> Vec<Range> {
     for range in ranges.iter().skip(1) {
         let mut found_overlap = false;
         for overlap in overlapped_ranges.iter_mut() {
-            match Range::from_overlapping(range, overlap) {
-                Some(r) => {
-                    *overlap = r.clone();
-                    found_overlap = true;
-                    no_overlaps = false;
-                }
-                None => (),
+            if let Some(r) = Range::from_overlapping(range, overlap) {
+                *overlap = r;
+                found_overlap = true;
+                no_overlaps = false;
             };
         }
         if !found_overlap {
@@ -140,26 +107,27 @@ fn get_overlapping_ranges(ranges: &[Range]) -> Vec<Range> {
     get_overlapping_ranges(&overlapped_ranges)
 }
 
-fn count_no_possible_beacons(
-    row: i32,
-    sensors: &[Sensor],
-    beacons: &[Point],
-    bounds: &Bounds,
-) -> usize {
+fn get_scanned_ranges_in_row(row: i64, sensors: &[Sensor]) -> Vec<Range> {
     let mut ranges = Vec::<Range>::new();
     for sensor in sensors.iter() {
-        match Range::from_sensor_row(&sensor, row) {
-            Some(r) => ranges.push(r),
-            None => (),
+        if let Some(r) = Range::from_sensor_row(sensor, row) {
+            ranges.push(r);
         };
     }
     // Get all the overlapping ranges
-    let overlaps = get_overlapping_ranges(&ranges);
+    let mut overlaps = get_overlapping_ranges(&ranges);
+    overlaps.sort_by(|a: &Range, b: &Range| a.start.cmp(&b.start));
     trace!("Overlapping ranges: {:?}", overlaps);
+    overlaps
+}
+
+fn count_not_possible_beacons(row: i64, sensors: &[Sensor], beacons: &[Point]) -> usize {
+    let scanned_ranges = get_scanned_ranges_in_row(row, sensors);
+    debug!("Scanned ranges: {:?}", scanned_ranges);
 
     // Can't be in scan lines
     let mut num_points: usize = 0;
-    for r in overlaps.iter() {
+    for r in scanned_ranges.iter() {
         num_points += (r.end - r.start) as usize + 1;
     }
 
@@ -168,15 +136,79 @@ fn count_no_possible_beacons(
         if b.y != row {
             continue;
         }
-        let mut inside_ranges = false;
-        for r in overlaps.iter() {
-            inside_ranges |= in_range(r.start, b.x, r.end);
+        let mut already_scanned = false;
+        for r in scanned_ranges.iter() {
+            already_scanned |= in_range(r.start, b.x, r.end);
         }
-        if inside_ranges {
+        if already_scanned {
             num_points -= 1;
         }
     }
     num_points
+}
+
+fn find_lost_beacon(bounds: Bounds, sensors: &[Sensor], beacons: &[Point]) -> Option<Point> {
+    for row in bounds.min_y..=bounds.max_y {
+        trace!("--- ROW {} ---", row);
+        let scanned_ranges: Vec<_> = get_scanned_ranges_in_row(row, sensors)
+            .iter_mut()
+            .filter(|r| bounds.min_x <= r.end && r.start <= bounds.max_x)
+            .map(|r| Range {
+                start: std::cmp::max(r.start, bounds.min_x),
+                end: std::cmp::min(r.end, bounds.max_x),
+            })
+            .collect();
+        if scanned_ranges.len() == 1
+            && scanned_ranges[0].end - scanned_ranges[0].start == bounds.max_x - bounds.min_x
+        {
+            continue;
+        }
+        debug!("Scanned ranges: {:?}", scanned_ranges);
+
+        let mut num_not_possible = 0;
+        for range in scanned_ranges.iter() {
+            num_not_possible += range.end - range.start;
+        }
+        for b in beacons.iter() {
+            if b.y != row {
+                continue;
+            }
+            let mut already_scanned = false;
+            for r in scanned_ranges.iter() {
+                already_scanned |= in_range(r.start, b.x, r.end);
+            }
+            if !already_scanned {
+                num_not_possible += 1;
+            }
+        }
+        let num_possible = (bounds.max_x - bounds.min_x) - num_not_possible;
+        debug!("Row {} has possible: {}", row, num_possible);
+        let mut candidates = Vec::<i64>::new();
+        for i in 1..scanned_ranges.len() {
+            let start = scanned_ranges[i - 1].end + 1;
+            let end = scanned_ranges[i].start - 1;
+            for j in start..=end {
+                candidates.push(j)
+            }
+        }
+        trace!("Candidate positions: {:?}", candidates);
+        for &col in candidates.iter() {
+            trace!("Checking point ({}, {})", col, row);
+            let mut already_beacon = false;
+            for b in beacons.iter() {
+                if b.y != row {
+                    continue;
+                }
+                already_beacon |= b.x == col;
+            }
+            if !already_beacon {
+                return Some(Point { x: col, y: row });
+            } else {
+                trace!("Beacon at ({}, {})", col, row);
+            }
+        }
+    }
+    None
 }
 
 fn parse_coord(s: &str) -> Point {
@@ -185,8 +217,8 @@ fn parse_coord(s: &str) -> Point {
     }
     let mut matches = POINT_RE.find_iter(s);
     Point {
-        x: matches.next().unwrap().as_str().parse::<i32>().unwrap(),
-        y: matches.next().unwrap().as_str().parse::<i32>().unwrap(),
+        x: matches.next().unwrap().as_str().parse::<i64>().unwrap(),
+        y: matches.next().unwrap().as_str().parse::<i64>().unwrap(),
     }
 }
 
@@ -240,6 +272,10 @@ fn setup_logger() {
 fn main() {
     setup_logger();
     let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() < 3 || 5 < args.len() {
+        println!("Args: <fname> row <i64> bound <i64>")
+    }
+
     let fname = &args[0];
     info!("Filename: {}", fname);
 
@@ -247,20 +283,42 @@ fn main() {
     trace!("{:?}", sensors);
     trace!("{:?}", beacons);
 
-    let bounds = Bounds::from_points(&beacons);
-    trace!("Min x: {}", bounds.min_x);
-    trace!("Max x: {}", bounds.max_x);
-    trace!("Min y: {}", bounds.min_y);
-    trace!("Max y: {}", bounds.max_y);
+    let mut i = 1;
+    while i < args.len() {
+        if i + 1 >= args.len() {
+            panic!("Expected argument for {}", args[i]);
+        }
+        let value = match args[i + 1].parse::<i64>() {
+            Ok(x) => x,
+            Err(e) => panic!("Could not parse int from \"{}\": {}", &args[i + 1], e),
+        };
+
+        if args[i] == "row" {
+            let row = value;
+            let num_no_beacon = count_not_possible_beacons(row, &sensors, &beacons);
+            info!("Part 1: {} positions can't have beacon.", num_no_beacon);
+        } else if args[i] == "bound" {
+            let max = value;
+            let bounds = Bounds {
+                min_x: 0,
+                max_x: max,
+                min_y: 0,
+                max_y: max,
+            };
+            match find_lost_beacon(bounds, &sensors, &beacons) {
+                Some(p) => {
+                    info!("Part 2: Beacon location: {:?}", p);
+                    info!("Part 2: Beacon frequency: {:?}", p.x * max + p.y);
+                }
+                None => error!("Part 2: Could not find beacon location!"),
+            };
+        } else {
+            println!("Unrecognized argument: \"{}\"", args[i]);
+        }
+        i += 2;
+    }
 
     if args.len() < 2 {
         panic!("No row specified");
     }
-    let row = match args[1].parse::<i32>() {
-        Ok(x) => x,
-        Err(e) => panic!("Could not parse row \"{}\": {}", &args[1], e),
-    };
-
-    let num_no_beacon = count_no_possible_beacons(row, &sensors, &beacons, &bounds);
-    info!("Part 1: {} positions can't have beacon.", num_no_beacon);
 }
