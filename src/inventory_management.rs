b56447a9e4d34c18b8d09b9704eec182a0d83909
MattMarti <matthew.marti@gmail.com>
2023-05-06 11:53:48 -0400

    Complete day 2 part 2

--- src/inventory_management.rs
+++ src/inventory_management.rs
@@ -45,9 +45,62 @@ fn compute_checksum(ids: &[String]) -> u64 {
     num_two_repeats * num_three_repeats
 }
 
+// Assume lengths are the same
+fn get_same_letters(a: &str, b: &str) -> String {
+    let mut same_chars = String::from("");
+    for (ac, bc) in a.chars().zip(b.chars()) {
+        if ac == bc {
+            same_chars.push(ac);
+        }
+    }
+    same_chars
+}
+
+fn find_similar_ids(args: &[String]) -> Option<String> {
+    for i in 0..args.len() {
+        for j in i..args.len() {
+            let same_chars = get_same_letters(&args[i], &args[j]);
+            if same_chars.len() + 1 == args[i].len() {
+                return Some(same_chars);
+            }
+        }
+    }
+    None
+}
+
 pub fn run(args: &[String]) {
     let ids = load_ids(&args[0]).unwrap();
 
     let checksum = compute_checksum(&ids);
-    println!("Checksum: {}", checksum)
+    println!("Checksum: {}", checksum);
+
+    let same_letters = find_similar_ids(&ids).unwrap();
+    println!("Same letters: \"{}\"", same_letters);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_get_same_letters() {
+        let a = "fghij";
+        let b = "fguij";
+        assert_eq!(get_same_letters(a, b), "fgij");
+    }
+
+    #[test]
+    fn test_find_similar() {
+        let input: Vec<String> = vec![
+            String::from("abcde"),
+            String::from("fghij"),
+            String::from("klmno"),
+            String::from("pqrst"),
+            String::from("fguij"),
+            String::from("axcye"),
+            String::from("wvxyz"),
+        ];
+        let id = find_similar_ids(&input).unwrap();
+        assert_eq!(id, "fgij");
+    }
 }
