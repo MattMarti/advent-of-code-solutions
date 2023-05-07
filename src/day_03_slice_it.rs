f932c6482799f6e32064aae5c7ab9b72772f1027
Matt Mart <matthew.marti@gmail.com>
2023-05-07 17:21:06 -0400

    Completed day 3 part 2

--- src/day_03_slice_it.rs
+++ src/day_03_slice_it.rs
@@ -1,11 +1,13 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 const NUM_SQAURES: usize = 1000;
 
 struct Claim {
+    id: usize,
     x: usize,
     y: usize,
     width: usize,
@@ -21,6 +23,7 @@ impl Claim {
             .unwrap();
         };
         RE.captures_iter(s).next().map(|caps| Self {
+            id: caps["id"].parse::<usize>().unwrap(),
             x: caps["x"].parse::<usize>().unwrap(),
             y: caps["y"].parse::<usize>().unwrap(),
             width: caps["width"].parse::<usize>().unwrap(),
@@ -44,32 +47,72 @@ fn load_claims(path: &str) -> io::Result<Vec<Claim>> {
     Ok(ids)
 }
 
-fn get_num_overlapped(claims: &[Claim]) -> usize {
-    let mut tiles = Vec::<Vec::<usize>>::new();
-    for _ in 0..NUM_SQAURES {
-        tiles.push(vec![0; NUM_SQAURES]);
+struct Panel {
+    tiles: Vec<Vec<Vec<usize>>>,
+}
+
+impl Panel {
+    pub fn from_claims(claims: &[Claim]) -> Self {
+        let mut tiles = Vec::<Vec<Vec<usize>>>::new();
+        for _ in 0..NUM_SQAURES {
+            tiles.push(vec![Vec::<usize>::new(); NUM_SQAURES]);
+        }
+        let mut panel = Self { tiles };
+        for c in claims {
+            for i in c.x..c.x + c.width {
+                for j in c.y..c.y + c.height {
+                    panel.tiles[i][j].push(c.id);
+                }
+            }
+        }
+        panel
     }
-    let mut num_overlaps = 0;
-    for c in claims {
-        for i in c.x..c.x + c.width {
-            for j in c.y..c.y + c.height {
-                tiles[i][j] += 1;
-                if tiles[i][j] == 2 {
+
+    fn get_num_overlapped(&self) -> usize {
+        let mut num_overlaps = 0;
+        for row in self.tiles.iter() {
+            for ids in row.iter() {
+                if ids.len() > 1 {
                     num_overlaps += 1;
                 }
             }
         }
+        num_overlaps
     }
-    num_overlaps
+
+    fn get_nonoverlapped_id(&self) -> Option<usize> {
+        let mut pure_ids = HashSet::<usize>::new();
+        for row in self.tiles.iter() {
+            for tile in row.iter() {
+                for id in tile {
+                    pure_ids.insert(*id);
+                }
+            }
+        }
+        for row in self.tiles.iter() {
+            for tile in row.iter() {
+                if tile.len() > 1 {
+                    for id in tile.iter() {
+                        pure_ids.remove(id);
+                    }
+                }
+            }
+        }
+        pure_ids.iter().next().copied()
+    }
 }
 
 pub fn run(args: &[String]) {
     let claims = load_claims(&args[0]).unwrap();
     println!("Found {} claims", claims.len());
 
+    let panel = Panel::from_claims(&claims);
 
-    let num_overlaps = get_num_overlapped(&claims);
+    let num_overlaps = panel.get_num_overlapped();
     println!("Number of overlaps: {}", num_overlaps);
+
+    let pure_id = panel.get_nonoverlapped_id().unwrap();
+    println!("Not overlapped id: {}", pure_id);
 }
 
 #[cfg(test)]
