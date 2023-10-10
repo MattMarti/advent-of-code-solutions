c0a1c6145503cfe6a434099a778a19d90a75ac2e
MattMarti <matthew.marti@gmail.com>
2023-10-09 22:26:27 -0400

    Solve day 4 part 1

--- src/day_04_repose_record.rs
+++ src/day_04_repose_record.rs
@@ -158,7 +158,7 @@ fn count_total_minutes_slept(log_entries: &[LogEntry]) -> HashMap<usize, usize> 
     sleep_count
 }
 
-fn find_most_minutes_slept(log_entries: &[LogEntry]) -> (usize, usize) {
+fn find_guard_with_most_sleep(log_entries: &[LogEntry]) -> (usize, usize) {
     let sleep_count = count_total_minutes_slept(log_entries);
     let mut most_sleep = 0;
     let mut sleepiest_guard = 0;
@@ -171,12 +171,116 @@ fn find_most_minutes_slept(log_entries: &[LogEntry]) -> (usize, usize) {
     (sleepiest_guard, most_sleep)
 }
 
+struct SleepGrid {
+    pub guard_ids: Vec<usize>,
+    pub asleep: Vec<Vec<bool>>,
+}
+
+impl SleepGrid {
+    fn new(log_entries: &[LogEntry]) -> Self {
+        let mut s = Self {
+            guard_ids: Vec::<usize>::default(),
+            asleep: Vec::<Vec<bool>>::default(),
+        };
+        let mut i = 0;
+        while i < log_entries.len() {
+            let entry = &log_entries[i];
+            s.guard_ids.push(entry.guard_id);
+
+            let j = i + get_shift_range(&log_entries[i..]);
+            s.asleep
+                .push(SleepGrid::get_sleep_flags(&log_entries[i..j]));
+
+            i = j;
+        }
+        s
+    }
+
+    fn get_sleep_flags(log_entries: &[LogEntry]) -> Vec<bool> {
+        let mut sleep_flags: Vec<bool> = vec![false; 120];
+        let mut last_asleep_time: &Timestamp = &log_entries[0].timestamp;
+        for entry in &log_entries[1..] {
+            if entry.action == GuardAction::FallAsleep {
+                last_asleep_time = &entry.timestamp;
+            } else if entry.action == GuardAction::WakeUp {
+                let start = SleepGrid::timestamp_to_sleep_index(last_asleep_time);
+                let end = SleepGrid::timestamp_to_sleep_index(&entry.timestamp);
+                for i in start..end {
+                    sleep_flags[i] = true;
+                }
+            }
+        }
+        sleep_flags
+    }
+
+    fn timestamp_to_sleep_index(t: &Timestamp) -> usize {
+        if t.hour == 23 {
+            t.minute
+        } else if t.hour == 0 {
+            t.minute + 60
+        } else {
+            panic!("Attempt to convert timestamp to index outside of expected range (23:00 to 1:00): {:02}:{:02}", t.hour, t.minute);
+        }
+    }
+
+    fn print(&self) {
+        for i in 0..self.guard_ids.len() {
+            print!("{:>4} ", self.guard_ids[i]);
+            for j in 0..self.asleep[i].len() {
+                if self.asleep[i][j] {
+                    print!("#");
+                } else {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+    }
+}
+
+fn find_most_slept_on_minute(grid: &SleepGrid, guard_id: usize) -> (usize, usize) {
+    let mut min_counts: Vec<usize> = vec![0; 120];
+    for i in 0..grid.guard_ids.len() {
+        if grid.guard_ids[i] != guard_id {
+            continue;
+        }
+        for j in 0..grid.asleep[i].len() {
+            if grid.asleep[i][j] {
+                min_counts[j] += 1;
+            }
+        }
+    }
+    let mut most_min_count = 0;
+    let mut most_min_index = 0;
+    for (i, &count) in min_counts.iter().enumerate() {
+        if count > most_min_count {
+            most_min_count = count;
+            most_min_index = i;
+        }
+    }
+    if most_min_index < 60 {
+        panic!("Most sleep found before midnight! Problem does not support this");
+    }
+    (most_min_index - 60, most_min_count)
+}
+
 pub fn run(args: &[String]) {
     let log_entries = load_log_entries(&args[0]).unwrap();
     println!("Loaded {} log entries", log_entries.len());
 
-    let (guard, amount) = find_most_minutes_slept(&log_entries);
+    let sleep_grid = SleepGrid::new(&log_entries);
+    sleep_grid.print();
+
+    let (guard, amount) = find_guard_with_most_sleep(&log_entries);
     println!("Guard {} slept the most at {} minutes.", guard, amount);
+
+    let (most_slept_time, amount) = find_most_slept_on_minute(&sleep_grid, guard);
+    println!(
+        "Most slept on minute was {} at {} times.",
+        most_slept_time, amount
+    );
+
+    println!("Part 1 hash: {}", guard * most_slept_time);
 }
 
 #[cfg(test)]
