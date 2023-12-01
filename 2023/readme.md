58a4d8e9558a64beace4367afee7144740140cb1
MattMarti <matthew.marti@gmail.com>
2023-11-30 22:29:11 -0500

    Start aoc 2023 with argument parser

---
+++ 2023/readme.md
@@ -0,0 +1,6 @@
+# Advent of Code 2023
+
+https://adventofcode.com/2023/
+
+I'm going to try these problems in Rust again. Hopefully this time
+I can try to do a better job of using visualizations.
---
+++ 2023/src/main.rs
@@ -0,0 +1,63 @@
+use std::collections::HashSet;
+use std::env;
+
+struct ProgramOption {
+    pub names: HashSet<String>,
+    pub func: fn(&[String]) -> (),
+}
+
+fn print_help(options: &[ProgramOption]) {
+    println!("Usage: main [problem] [input file]");
+    println!("Arguments: ");
+    for opt in options {
+        print!("  ");
+        let mut sep = "";
+        for name in opt.names.iter() {
+            print!("{sep}{name}");
+            sep = ", ";
+        }
+        println!();
+    }
+}
+
+fn foo(args: &[String]) {
+    println!("Yay");
+    let mut sep = "";
+    for a in args {
+        print!("{sep}{a}");
+        sep = ", ";
+    }
+    println!();
+}
+
+#[macro_export]
+macro_rules! cmdset {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push(format!("{}", $x));
+            )*
+            temp_vec.into_iter().collect()
+        }
+    };
+}
+
+fn main() {
+    let options = vec![ProgramOption {
+        names: cmdset!["foo"],
+        func: foo,
+    }];
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.len() < 2 {
+        print_help(&options);
+        return;
+    }
+    let cmd = &args[0];
+    if let Some(opt) = options.iter().find(|&o| o.names.contains(cmd)) {
+        (opt.func)(&args[1..]);
+    } else {
+        println!("Unrecognized argument: {cmd}");
+        print_help(&options);
+    }
+}
