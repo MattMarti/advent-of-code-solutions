42c1b4f54637308431476e76b5130359d55c6180
MattMarti <matthew.marti@gmail.com>
2023-12-02 17:09:07 -0500

    Solved part 2

--- 2023/src/dec_02_cube_conundrum.rs
+++ 2023/src/dec_02_cube_conundrum.rs
@@ -4,7 +4,6 @@ use regex::Regex;
 
 use crate::load_file_lines;
 
-
 #[derive(Default, Debug)]
 struct CubeCounts {
     red: usize,
@@ -15,9 +14,8 @@ struct CubeCounts {
 impl CubeCounts {
     pub fn from_str(line: &str) -> Self {
         lazy_static! {
-            static ref RE_COLORS: Regex = Regex::new(
-                r"(?<count>\d+) (?<color>((blue)|(red)|(green)))"
-            ).unwrap();
+            static ref RE_COLORS: Regex =
+                Regex::new(r"(?<count>\d+) (?<color>((blue)|(red)|(green)))").unwrap();
         };
         let mut obj = Self::default();
         for cap in RE_COLORS.captures_iter(line) {
@@ -27,7 +25,7 @@ impl CubeCounts {
                 "red" => obj.red = count,
                 "green" => obj.green = count,
                 "blue" => obj.blue = count,
-                _ => println!("WARNING: Bad color {}", color)
+                _ => println!("WARNING: Bad color {}", color),
             }
         }
         obj
@@ -36,7 +34,7 @@ impl CubeCounts {
 
 // game > round > set
 fn load_game_results(path: &str) -> Vec<Vec<CubeCounts>> {
-    let mut game_results = Vec::<Vec::<CubeCounts>>::new();
+    let mut game_results = Vec::<Vec<CubeCounts>>::new();
     for line in load_file_lines(path).unwrap().iter() {
         let mut round_results = Vec::<CubeCounts>::new();
 
@@ -48,8 +46,7 @@ fn load_game_results(path: &str) -> Vec<Vec<CubeCounts>> {
         }
         let num_chars = line.chars().count();
         split_idxs.push(num_chars);
-        for (&start, &end) in zip(split_idxs.iter().take(num_chars), split_idxs.iter().skip(1))
-        {
+        for (&start, &end) in zip(split_idxs.iter().take(num_chars), split_idxs.iter().skip(1)) {
             let substr = line.chars().take(end).skip(start).collect::<String>();
             round_results.push(CubeCounts::from_str(&substr));
         }
@@ -58,18 +55,56 @@ fn load_game_results(path: &str) -> Vec<Vec<CubeCounts>> {
     game_results
 }
 
+fn is_possible(candidate: &CubeCounts, actual: &CubeCounts) -> bool {
+    actual.red <= candidate.red && actual.green <= candidate.green && actual.blue <= candidate.blue
+}
 
 pub fn run(args: &[String]) {
     let game_results = load_game_results(&args[0]);
+
+    let part_1_candidate = CubeCounts {
+        red: 12,
+        green: 13,
+        blue: 14,
+    };
 
     if args.contains(&String::from("debug")) {
         for (i, round) in game_results.iter().enumerate() {
             println!("Game {i}:");
             for res in round.iter() {
-                println!("- {:?}", res);
+                print!("- {:?}", res);
+                if !is_possible(&part_1_candidate, res) {
+                    print!(" - IMPOSSIBLE");
+                }
+                println!();
             }
         }
     }
 
+    // Find impossible games
+    let mut part_1_checksum = 0;
+    for (i, round) in game_results.iter().enumerate() {
+        if !round.iter().any(|res| !is_possible(&part_1_candidate, res)) {
+            part_1_checksum += i + 1;
+        }
+    }
+    println!("Part 1 checksum: {}", part_1_checksum);
 
+    // Find fewest cubes
+    let mut part_2_checksum = 0;
+    for round in game_results.iter() {
+        let mut fewest_counts = CubeCounts {
+            red: 0,
+            green: 0,
+            blue: 0,
+        };
+        for result in round {
+            fewest_counts.red = usize::max(fewest_counts.red, result.red);
+            fewest_counts.green = usize::max(fewest_counts.green, result.green);
+            fewest_counts.blue = usize::max(fewest_counts.blue, result.blue);
+        }
+        let power = fewest_counts.red * fewest_counts.green * fewest_counts.blue;
+        part_2_checksum += power;
+    }
+    println!("Part 2 checksum: {}", part_2_checksum);
 }
