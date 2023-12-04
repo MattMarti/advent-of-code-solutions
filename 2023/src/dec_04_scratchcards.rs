e7f7e24e1744a2b3095a2e6003484cc110774c3e
Matt Mart <matthew.marti@gmail.com>
2023-12-04 15:24:56 -0500

    Solve day 4 part 2

--- 2023/src/dec_04_scratchcards.rs
+++ 2023/src/dec_04_scratchcards.rs
@@ -44,42 +44,48 @@ fn print_first_part(line: &str) -> Result<(), &'static str> {
 
 pub fn run(args: &[String]) {
     let lines = load_file_lines(&args[0]).unwrap();
+    let mut card_copy_counts: Vec<i32> = vec![0; lines.len()];
     let mut total_score = 0;
     for (i, line) in lines.iter().enumerate() {
         // Print first half
-        if let Err(msg) = print_first_part(&line) {
+        if let Err(msg) = print_first_part(line) {
             println!("Error on line {i}: {}", msg);
             continue;
         }
 
         // Print winning numbers
         let card = Card::from_str(line);
-        let mut score = 0;
-        let mut max_score = 1;
+        let mut match_count = 0;
         for win_num in card.winning_nums.iter() {
             let output = format!("{:2} ", win_num);
-            max_score *= 2;
             if card.user_nums.contains(win_num) {
-                if score == 0 {
-                    score = 1;
-                } else {
-                    score *= 2;
-                }
+                match_count += 1;
                 print!("{}", output.green());
             } else {
                 print!("{}", output);
             }
         }
-        max_score /= 2;
+        let score = if match_count > 0 {
+            1 << (match_count - 1)
+        } else {
+            0
+        };
+        let max_score = 1 << card.winning_nums.len();
         if score == max_score {
             print!("\r");
-            let _ = print_first_part(&line).unwrap();
+            print_first_part(line).unwrap();
             for num in card.winning_nums.iter() {
                 let s = format!("{:2} ", num);
                 print!("{}", s.blue());
             }
         }
         total_score += score;
+
+        // Part 2: Calculate card counts
+        let num_copies = 1 + card_copy_counts[i];
+        for n in card_copy_counts.iter_mut().skip(i + 1).take(match_count) {
+            *n += num_copies;
+        }
 
         // Print user numbers
         print!("|");
@@ -93,10 +99,16 @@ pub fn run(args: &[String]) {
         }
 
         // Finish
-        println!(" ({score})");
+        println!(
+            " (matches {match_count}, score {score}, copies {})",
+            card_copy_counts[i]
+        );
     }
-
-    println!("Total score: {total_score}");
+    println!("Total score (part 1): {total_score}");
+    println!(
+        "Num cards (part 2): {}",
+        card_copy_counts.iter().map(|n| n + 1).sum::<i32>()
+    );
 }
 
 #[cfg(test)]
