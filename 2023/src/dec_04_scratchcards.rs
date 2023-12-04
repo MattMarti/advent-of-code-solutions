0db4d977a48efd23848e802317d508e472dba47f
Matt Mart <matthew.marti@gmail.com>
2023-12-04 13:25:21 -0500

    Solve day 4 part 1

--- 2023/src/dec_04_scratchcards.rs
+++ 2023/src/dec_04_scratchcards.rs
@@ -12,8 +12,8 @@ struct Card {
 
 impl Card {
     pub fn from_str(s: &str) -> Self {
-        let win_num_start = s.chars().position(|c| c == ':').unwrap() + 2;
-        let win_num_end = s.chars().position(|c| c == '|').unwrap() - 2;
+        let win_num_start = s.chars().position(|c| c == ':').unwrap() + 1;
+        let win_num_end = s.chars().position(|c| c == '|').unwrap() - 1;
         let user_num_start = s.chars().position(|c| c == '|').unwrap() + 1;
         Self {
             winning_nums: s.chars().into_iter()
@@ -39,13 +39,37 @@ pub fn run(args: &[String]) {
     let cards: Vec<Card> = lines.iter().map(|s| Card::from_str(&s)).collect();
 
     let mut total_score = 0;
-    for c in cards.iter() {
-        let mut count = 0;
+    for (i, c) in cards.iter().enumerate() {
+        let mut score = 0;
         for win_num in c.winning_nums.iter() {
             if c.user_nums.contains(win_num) {
-                count += 1;
+                if score == 0 { score = 1;}
+                else { score *= 2;}
             }
         }
+        println!("Card {}: {}", i + 1, score);
+        total_score += score;
     }
+    println!("Card score: {total_score}");
     println!("{} {}", "merry".red(), "christmas!".green());
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let line = "Card 1: 3 6 4 | 5 82 23 3 54 6";
+        let card = Card::from_str(&line);
+        assert!(card.winning_nums.contains(&3));
+        assert!(card.winning_nums.contains(&6));
+        assert!(card.winning_nums.contains(&4));
+        assert!(card.user_nums.contains(&5));
+        assert!(card.user_nums.contains(&82));
+        assert!(card.user_nums.contains(&23));
+        assert!(card.user_nums.contains(&3));
+        assert!(card.user_nums.contains(&54));
+        assert!(card.user_nums.contains(&6));
+    }
 }
