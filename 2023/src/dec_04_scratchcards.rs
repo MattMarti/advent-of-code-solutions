4ac4dbb24200e86dab8945c11c7510246d644060
Matt Mart <matthew.marti@gmail.com>
2023-12-04 13:58:20 -0500

    Highlight winners in blue

--- 2023/src/dec_04_scratchcards.rs
+++ 2023/src/dec_04_scratchcards.rs
@@ -33,23 +33,32 @@ impl Card {
     }
 }
 
+fn print_first_part(line: &str) -> Result<(), &'static str> {
+    if let Some(idx) = line.chars().position(|c| c == ':') {
+        print!("{}: ", line.chars().take(idx).collect::<String>());
+    } else {
+        return Err("Parse error: Line is missing colon.");
+    }
+    Ok(())
+}
+
 pub fn run(args: &[String]) {
     let lines = load_file_lines(&args[0]).unwrap();
     let mut total_score = 0;
     for (i, line) in lines.iter().enumerate() {
         // Print first half
-        if let Some(idx) = line.chars().position(|c| c == ':') {
-            print!("{}: ", line.chars().take(idx).collect::<String>());
-        } else {
-            println!("Parse error on line {i}");
+        if let Err(msg) = print_first_part(&line) {
+            println!("Error on line {i}: {}", msg);
             continue;
         }
 
         // Print winning numbers
         let card = Card::from_str(line);
         let mut score = 0;
+        let mut max_score = 1;
         for win_num in card.winning_nums.iter() {
             let output = format!("{:2} ", win_num);
+            max_score *= 2;
             if card.user_nums.contains(win_num) {
                 if score == 0 {
                     score = 1;
@@ -59,6 +68,15 @@ pub fn run(args: &[String]) {
                 print!("{}", output.green());
             } else {
                 print!("{}", output);
+            }
+        }
+        max_score /= 2;
+        if score == max_score {
+            print!("\r");
+            let _ = print_first_part(&line).unwrap();
+            for num in card.winning_nums.iter() {
+                let s = format!("{:2} ", num);
+                print!("{}", s.blue());
             }
         }
         total_score += score;
